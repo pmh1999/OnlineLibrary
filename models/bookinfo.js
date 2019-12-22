99162f8a4fa21eec16021767a2f019133fda0ec2
Nguyen Huy Hoang <hoanggb11@gmail.com>
2019-12-22 13:30:03 +0700

    fixx database

--- models/bookinfo.js
+++ models/bookinfo.js
@@ -3,13 +3,16 @@ module.exports = (sequelize, DataTypes) => {
   const BookInfo = sequelize.define('BookInfo', {
     isbn: DataTypes.STRING,
     title: DataTypes.STRING,
+    category: DataTypes.STRING,
+    author: DataTypes.STRING,
     publisher: DataTypes.STRING,
     publishYear: DataTypes.DECIMAL,
     language: DataTypes.STRING,
     haveCD: DataTypes.BOOLEAN,
     amount: DataTypes.DECIMAL,
     imagePath: DataTypes.TEXT,
-    thumbnailPath: DataTypes.TEXT
+    thumbnailPath: DataTypes.TEXT,
+    description: DataTypes.TEXT
   }, {});
   BookInfo.associate = function(models) {
     // associations can be defined here
