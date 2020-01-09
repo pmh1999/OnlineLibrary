9693d204c38541b7e80289953e6c9324e79a132f
Nguyen Huy Hoang <hoanggb11@gmail.com>
2020-01-09 22:16:27 +0700

    add avtPath

--- seeders/20191220093034-demo-user.js
+++ seeders/20191220093034-demo-user.js
@@ -8,21 +8,21 @@ module.exports = {
       "password": "$2a$10$k2jFLww0B44C4WhEP0sC9OyOG0ZQietD91.v0yFI28eEwMP/.idAa",
       "fullname": "Admin",
       "phone": "774-996-9938",
-      "avatarPath": "http://dummyimage.com/110x244.jpg/dddddd/000000",
+      "avatarPath": "/img/avt3.png",
       "isAdmin": true
     }, {
-      "username": "sburel9@sciencedaily.com",
-      "password": "VkY0aOVsR",
-      "fullname": "Shawn Burel",
+      "username": "test@gmail.com",
+      "password": "$2a$10$lrTkF9fzwXV.gtgboQg3BeIk7TSBfyRTDXTUT9ZdI2OEldXplQ7qe",
+      "fullname": "Test Account",
       "phone": "789-159-0941",
-      "avatarPath": "http://dummyimage.com/136x204.jpg/dddddd/000000",
+      "avatarPath": "/img/avt1.png",
       "isAdmin": false
     },{
-      "username": "sburel0@sciencedaily.com",
-      "password": "VkY0aOVsR",
-      "fullname": "Shawn 1",
+      "username": "abc@gmail.com",
+      "password": "$2a$10$bzvWf88D5aNbWjJX1teXnOwVtqI6BWU/fH/C9EjcbUAOfDTwTjE4C",
+      "fullname": "ABC Acc",
       "phone": "789-159-0941",
-      "avatarPath": "http://dummyimage.com/136x204.jpg/dddddd/000000",
+      "avatarPath": "/img/avt2.jpg",
       "isAdmin": false
     },{
       "username": "sburel1@sciencedaily.com",
