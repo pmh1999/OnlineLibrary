41e6c6381c36ce9b0871f1bdc2c3daec7fbe5bce
1753024 <an.tranquoctruong@gmail.com>
2020-01-09 12:59:42 +0700

    change

--- router/AdminBookRouter.js
+++ router/AdminBookRouter.js
@@ -42,6 +42,48 @@ router.get('/tablebook',(req,res,next)=>{
         .catch(error=>{next(error);})
 });
 
+router.get('/tableauthor',(req,res,next)=>{
+    if ((req.query.sort == null)) {
+        req.query.sort = 'id';
+    }
+    
+    if ((req.query.limit == null) || isNaN(req.query.limit)){
+        req.query.limit = 20;
+    }
+    
+    if ((req.query.search == null) || (req.query.search.trim() == '')) {
+        req.query.search = '';
+    }
+
+    if ((req.query.type == null) || (req.query.type.trim() == '')) {
+        req.query.type = '';
+    }
+    
+    if ((req.query.page == null) || isNaN(req.query.page)){
+        req.query.page = 1;
+    }
+
+    let bookcontroller=require('../controllers/bookcontroller');
+    bookcontroller
+        .getAll(req.query)
+        .then(data =>{
+            res.locals.book =data.rows;
+            res.locals.pagination = {
+                page: parseInt(req.query.page),
+                limit: parseInt(req.query.limit),
+                totalRows: data.count
+            };
+            res.render('admin/tableAthour');
+            
+        })
+        .catch(error=>{next(error);})
+});
+
+router.get('/chart',(req,res,next)=>{
+    res.render('admin/charts');
+});
+
+
 
 
 module.exports = router;
